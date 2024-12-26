ce599258b741093080b2b4253a86caddf6f71791
Piotr Wlosek <piotr.wlosekx@gmail.com>
2024-12-26 12:48:01 +0100

    bugfix/v1.0.5 - fix issue with empty value, add serializer

--- ViewModel/Config.php
+++ ViewModel/Config.php
@@ -12,6 +12,12 @@ use Magento\Framework\Serialize\Serializer\Json;
 
 class Config implements ArgumentInterface
 {
+    public const ALL_FORMS = 0;
+
+    public const SHIPPING_FORMS = 1;
+
+    public const BILLING_FORMS = 2;
+
     protected const M2S_ENABLE_MODULE_PATH = 'm2s/general/enabled';
 
     protected const M2S_ADVANCED_SHIPPING_ADDRESS_PATH = 'm2s/advanced/shipping_address_path';
@@ -23,12 +29,6 @@ class Config implements ArgumentInterface
     protected const M2S_CUSTOM_FIELDS_VALIDATION_JSON_PATH = 'm2s/general/fields_validation';
 
     protected const CHECKOUT_DISPLAY_BILLING_ADDRESS = 'checkout/options/display_billing_address_on';
-
-    public const ALL_FORMS = 0;
-
-    public const SHIPPING_FORMS = 1;
-
-    public const BILLING_FORMS = 2;
 
     /**
      * @param ScopeConfigInterface $scopeConfig
@@ -96,6 +96,9 @@ class Config implements ArgumentInterface
         return $result;
     }
 
+    /**
+     * @return array
+     */
     public function getCustomFieldsValidationJson(): array
     {
         $values = $this->scopeConfig->getValue(
@@ -108,7 +111,7 @@ class Config implements ArgumentInterface
 
         return array_map(function ($value) {
             return $value;
-        }, is_array($values) ? $values : json_decode($values, true));
+        }, is_array($values) ? $values : $this->serializer->unserialize($values));
     }
 
 
